f8e4bc3a7da8dfd960fb44dddd8cf8783cdc3a94
Florian Schmidt <flo@devflo.de>
2024-05-13 19:06:20 +0200

    Update config.ts

--- server/config.ts
+++ server/config.ts
@@ -1,6 +1,6 @@
 export const DiscordAuthConfig = {
     APPLICATION_ID: '',
-    SERVER_ID: '',
-    WHITELIST_ROLE_ID: '',
+    SERVER_ID: '', // leave empty if do not use whitelist | REQUIRED rebar-discord
+    WHITELIST_ROLE_ID: '', // leave empty if do not use whitelist | REQUIRED rebar-discord
     SESSION_EXPIRE_TIME_IN_MINUTES: 3,
 }
